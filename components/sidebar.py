82ff42555b26acd614ecd6c6a80a89860f9048ad
tylertaewook <70506321+tylertaewook@users.noreply.github.com>
2023-12-27 22:00:39 +0900

    added llama-indexing

---
+++ components/sidebar.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+def sidebar():
+    with st.sidebar:
+
+        st.markdown("---")
+        st.markdown("# About")
+        st.markdown(
+            "📖KnowledgeGPT allows you to ask questions about your "
+            "documents and get accurate answers with instant citations. "
+        )
+        st.markdown(
+            "This tool is a work in progress. "
+            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+            "with your feedback and suggestions💡"
+        )
+        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("---")
+
+        faq()
---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+st.set_page_config(page_title="HanwhaGPT", page_icon="📁", layout="centered", initial_sidebar_state="auto", menu_items=None)
+# openai.api_key = st.secrets.openai_key
+openai.api_key = "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"
+# openai.api_key = st.session_state.get("OPENAI_API_KEY")
+st.title("HanwhaGPT")
+st.info("Built by [tylertaewook](https://tylertaewook.com) for Hanwha GIP AI Track's Case Study. View source code at [Github](https://github.com/tylertaewook/hanwha-ai-case-study)", icon="📃")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about President Biden's State of the Union address in 2022!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", temperature=0.5, system_prompt="You are an expert on the the content of President Biden's State of the Union address in 2022. You are aware of the United States' position on Russia's invasion of Ukraine, as well as the economic achievements of the past year and future plans. Assume that all questions are related to the President Biden's speech. Keep your answers based on facts – do not hallucinate features. When asked unrelated question, refuse to answer."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
